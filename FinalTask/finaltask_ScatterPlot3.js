bde077cb82617e725f4ee921457c90263e04d519
Ryosuke Suzuki <ryosuek.3.11.98@gmail.com>
2021-06-13 19:19:10 +0900

    data

--- FinalTask/finaltask_ScatterPlot3.js
+++ FinalTask/finaltask_ScatterPlot3.js
@@ -1,4 +1,4 @@
-class ScatterPlot1 {
+class ScatterPlot3 {
 
     constructor( config, data ) {
         this.config = {
--- FinalTask/finaltask_ScatterPlot4.js
+++ FinalTask/finaltask_ScatterPlot4.js
@@ -1,4 +1,4 @@
-class ScatterPlot1 {
+class ScatterPlot4 {
 
     constructor( config, data ) {
         this.config = {
