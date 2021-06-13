bc75ed5c81ec5be51181d65ef1f66884fcc48fe1
Ryosuke Suzuki <ryosuek.3.11.98@gmail.com>
2021-06-13 19:18:11 +0900

    data

--- FinalTask/finaltask_ScatterPlot1.js
+++ FinalTask/finaltask_ScatterPlot1.js
@@ -97,7 +97,7 @@ class ScatterPlot1 {
         circles
             .attr("r", circle_radius )
             .attr("cx", d => self.xscale( self.xvalue(d) ) )
-            .attr("cy", d => self.yscale( self.yvalue(d) ) );
+            .attr("cy", d => self.yscale( self.yvalue(d) ) )
             .attr("fill", d => self.config.cscale( self.cvalue(d) ) );
 
         circles
